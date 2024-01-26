95053ce7844953b268faecf4dd736f650a07a1fe
Artem <artem.smelyanskiy81@gmail.com>
2024-01-26 22:46:50 +0200

    finished adaptive mobile-window

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const modalBackdrop = document.getElementById("modalBackdrop");
   const closeModalBtn = document.getElementById("closeModalBtn");
 
+  const menuMobileBtn = document.getElementById("menuMobileBtn");
+  const closeMobileBtn = document.getElementById("closeMobileBtn");
+
   openModalBtn.addEventListener("click", function () {
     modalBackdrop.classList.add("is-open");
   });
@@ -11,9 +14,16 @@ document.addEventListener("DOMContentLoaded", function () {
   closeModalBtn.addEventListener("click", function () {
     modalBackdrop.classList.remove("is-open");
   });
+
+  menuMobileBtn.addEventListener('click', function () {
+    mobileWindow.classList.add("is-open");
+  });
+
+  closeMobileBtn.addEventListener("click", function () {
+    mobileWindow.classList.remove("is-open");
+  });
+
 });
-
-
 /*for (const item of showModalWindow) {
     item.querySelector(".modal-btn-close").onclick = () => item.remove();
   }
